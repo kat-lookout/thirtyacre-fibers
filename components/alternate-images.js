e277db915f36f20af665f8b6f2ac747327640a99
kat-lookout <47338876+kat-lookout@users.noreply.github.com>
2022-03-02 16:30:36 -0500

    Add click to embiggen
    Larger / higher quality jpegs

--- components/alternate-images.js
+++ components/alternate-images.js
@@ -3,7 +3,7 @@ import OtherImage from '/components/other-image'
 export default function AlternateImages({ title, images }) {
     return (
         <div className="flex flex-row justify-start content-start h-auto min-h-fit">
-            {images.map((image) => 
+            {images.map((image) =>
                 <OtherImage
                     title={title}
                     src={image}
--- components/other-image.js
+++ components/other-image.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '/components/image-styles.module.css'
 
-export default function OtherImage({ title, src, height, width }) {    
+export default function OtherImage({ title, src, height, width }) {
     return (
         <div className="w-1/3">
-            <Image src={src} alt={`Alternate view of ${title}`} layout="responsive" height={height} width={width} className={styles.overlap} />
+            <Link href={src}><a><Image src={src} alt={`Alternate view of ${title}`} layout="responsive" height={height} width={width} className={styles.overlap} /></a></Link>
         </div>
     )
 }
