ccec0ec56d500364a5c947ee56deff3705e79461
kat-lookout <47338876+kat-lookout@users.noreply.github.com>
2022-11-17 13:09:21 -0500

    On branch master
    Changes to be committed:
    	modified:   components/meta.js
    	new file:   pages/api/rss.js
    
    Trying a different approach here.

---
+++ pages/api/rss.js
@@ -0,0 +1,41 @@
+const importBlogPosts = async () => {
+    const markdownFiles = require.context('/content/blogPosts', false, /\.md$/).keys().map((relativePath) => relativePath.substring(2))
+
+    return Promise.all(
+        markdownFiles.map(async (path) => {
+            const markdown = await import(`/content/blogPosts/${path}`)
+            return { ...markdown, slug: path.substring(0, path.length - 3) }
+        })
+    )
+}
+
+export default async function handler(req, res) {
+    try {
+        const posts = await importBlogPosts()
+        const feedItems = posts.map((item) => {
+            return `
+            <item>
+                <title>${item.attribute.title}</title>
+                <author>Kat Wenger</author>
+                <link>https://www.thirtyacrefibers.com/blog/post/${item.slug}</link>
+                <guid>https://www.thirtyacrefibers.com/blog/post/${item.slug}</guid>
+                <pubDate>${item.attributes.date}</pubDate>
+            </item>`;
+        }).join("")
+        const feed = `
+        <?xml version="1.0"?>
+        <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+            <channel>
+                <title>Thirtyacre Fibers</title>
+                <atom:link href="https://www.thiryacrefibers.com/api/rss.js" rel="self" type="application/rss+xml" />
+                <link>https://www.thirtyacrefibers.com</link>
+                <description>Fiber artits. Software developer. Cat lover. General nerd.</description>
+                ${feedItems}
+            </channel>
+        </rss>`
+        res.status(200).send(feed);
+    }
+    catch (err) {
+        res.status(500).send({ error : 'Unable to load feed.' });
+    }
+}
