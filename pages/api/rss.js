12a94f7056fe4e9b50b1e83d3958c2965b72ff7e
kat-lookout <47338876+kat-lookout@users.noreply.github.com>
2022-11-17 13:26:59 -0500

    On branch master
    Changes to be committed:
    	modified:   pages/api/rss.js

--- pages/api/rss.js
+++ pages/api/rss.js
@@ -10,32 +10,27 @@ async function importBlogPosts () {
 }
 
 export default async function handler(req, res) {
-    try {
-        const posts = await importBlogPosts()
-        const feedItems = posts.map((item) => {
-            return `
-            <item>
-                <title>${item.attribute.title}</title>
-                <author>Kat Wenger</author>
-                <link>https://www.thirtyacrefibers.com/blog/post/${item.slug}</link>
-                <guid>https://www.thirtyacrefibers.com/blog/post/${item.slug}</guid>
-                <pubDate>${item.attributes.date}</pubDate>
-            </item>`;
-        }).join("")
-        const feed = `
-        <?xml version="1.0"?>
-        <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-            <channel>
-                <title>Thirtyacre Fibers</title>
-                <atom:link href="https://www.thiryacrefibers.com/api/rss.js" rel="self" type="application/rss+xml" />
-                <link>https://www.thirtyacrefibers.com</link>
-                <description>Fiber artits. Software developer. Cat lover. General nerd.</description>
-                ${feedItems}
-            </channel>
-        </rss>`
-        res.status(200).send(feed);
-    }
-    catch (err) {
-        res.status(500).send(err);
-    }
+    const posts = await importBlogPosts()
+    const feedItems = posts.map((item) => {
+        return `
+        <item>
+            <title>${item.attribute.title}</title>
+            <author>Kat Wenger</author>
+            <link>https://www.thirtyacrefibers.com/blog/post/${item.slug}</link>
+            <guid>https://www.thirtyacrefibers.com/blog/post/${item.slug}</guid>
+            <pubDate>${item.attributes.date}</pubDate>
+        </item>`;
+    }).join("")
+    const feed = `
+    <?xml version="1.0"?>
+    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+        <channel>
+            <title>Thirtyacre Fibers</title>
+            <atom:link href="https://www.thiryacrefibers.com/api/rss.js" rel="self" type="application/rss+xml" />
+            <link>https://www.thirtyacrefibers.com</link>
+            <description>Fiber artits. Software developer. Cat lover. General nerd.</description>
+            ${feedItems}
+        </channel>
+    </rss>`
+    res.status(200).send(feed);
 }
