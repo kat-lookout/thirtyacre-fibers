ad6390133fe62b2cec6033df561b06980ac16dd3
kat-lookout <47338876+kat-lookout@users.noreply.github.com>
2022-11-18 10:39:19 -0500

    On branch master
    Changes to be committed:
    	modified:   pages/api/rss.js

--- pages/api/rss.js
+++ pages/api/rss.js
@@ -21,36 +21,36 @@ export default async function handler(req, res) {
         const content = item.default.html.replace(/<(\s*(img|br)[^>]*)>/g, "<$1 />")
 
         return `
-        <entry>
-            <id>https://www.thirtyacrefibers.com/blog/post/${item.slug}</id>
-            <title>${item.attributes.title}</title>
-            <updated>${item.attributes.date}</updated>
-            <author>
-                <name>Kat Wenger</name>
-            </author>
-            <content type="xhtml">
-                <div xmlns="http://www.w3.org/1999/xhtml">
-                    ${content}
-                </div>
-            </content>
-            <link rel="alternate" href="https://www.thirtyacrefibers.com/blog/post/${item.slug}" />
-        </entry>`;
+    <entry>
+        <id>https://www.thirtyacrefibers.com/blog/post/${item.slug}</id>
+        <title>${item.attributes.title}</title>
+        <updated>${item.attributes.date}</updated>
+        <author>
+            <name>Kat Wenger</name>
+        </author>
+        <content type="xhtml">
+            <div xmlns="http://www.w3.org/1999/xhtml">
+                ${content}
+            </div>
+        </content>
+        <link rel="alternate" href="https://www.thirtyacrefibers.com/blog/post/${item.slug}" />
+    </entry>`;
     }).join("")
 
     const feed = `<?xml version="1.0" encoding="utf-8"?>
-    <feed xmlns="http://www.w3.org/2005/Atom">
-        <id>https://www.thirtyacrefibers.com/</id>
-        <title type="text">Thirtyacre Fibers</title>
-        <updated>${new Date().toISOString()}</updated>
-        <author>
-            <name>Kat Wenger</name>
-            <email>thirtyacrefibers@gmail.com</email>
-            <uri>https://www.thirtyacrefibers.com/about</uri>
-        </author>
-        <link href="https://www.thirtyacrefibers.com/api/rss" rel="self" type="application/atom+xml" />
-        <icon>https://www.thirtyacrefibers.com/imag/smallLogo.png</icon>
-        <subtitle type="text">Fiber artist. Software developer. Cat lover. General nerd.</subtitle>
-        ${feedItems}
-    </feed>`
+<feed xmlns="http://www.w3.org/2005/Atom">
+    <id>https://www.thirtyacrefibers.com/</id>
+    <title type="text">Thirtyacre Fibers</title>
+    <updated>${new Date().toISOString()}</updated>
+    <author>
+        <name>Kat Wenger</name>
+        <email>thirtyacrefibers@gmail.com</email>
+        <uri>https://www.thirtyacrefibers.com/about</uri>
+    </author>
+    <link href="https://www.thirtyacrefibers.com/api/rss" rel="self" type="application/atom+xml" />
+    <icon>https://www.thirtyacrefibers.com/imag/smallLogo.png</icon>
+    <subtitle type="text">Fiber artist. Software developer. Cat lover. General nerd.</subtitle>
+    ${feedItems}
+</feed>`
     res.status(200).send(feed);
 }
